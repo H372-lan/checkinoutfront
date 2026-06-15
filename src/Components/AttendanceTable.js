f26d4128cc592267d4d9ac93ea0dbe5d2c767ffc
Harshita Bajpai <78996496+H372-lan@users.noreply.github.com>
2026-06-15 16:26:36 +0530

    2

--- src/Components/AttendanceTable.js
+++ src/Components/AttendanceTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const AttendanceTable = ({ data }) => {
+const AttendanceTable = ({ data }) => {  
     return (
         <table className="table table-striped">
             <thead>
